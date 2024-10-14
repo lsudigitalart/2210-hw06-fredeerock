73e6f09fd234541a9c2a33c15c22e5524969375f
Derick Ostrenko <dostrenko@gmail.com>
2024-10-14 13:53:36 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,45 +1,46 @@
-let imgElmo, randomCat, imgCat, imgPlaceX, imgPlaceY, pressed, imgRocket, imgRocketScalar, imgRocketSize, imgMoon, imgMoonScalar, imgMoonSize, imgMonster, imgMonsterScalar, imgMonsterSize;
+let jack, pump, rand, m, d, s, lorem, myIP;
 
 function preload() {
-  imgRocket = loadImage("rocket.avif");
-  imgMoon = loadImage("moon.webp");
-  imgMonster = loadImage("monster.gif");
-  imgCat = loadImage("cat.gif")
-  imgElmo = loadImage("elmo.gif")
-  pressed = false;
+  jack = loadImage("https://png.pngtree.com/png-clipart/20230417/ourmid/pngtree-halloween-glowing-jack-o-lantern-png-image_6707380.png")
+  pump = loadImage("https://cdn.pixabay.com/photo/2014/04/03/09/58/pumpkin-309453_640.png")
+  lorem = loadImage("https://picsum.photos/200")
 }
 
 function setup() {
   createCanvas(400, 400);
-  imgRocketScalar = 3072/1728;
-  imgRocketSize = 400;
-  imgMoonScalar = 4096/2731;
-  imgMoonSize = 100;
-  imgMonsterScalar = 4096/2731;
-  imgMonsterSize = 50;
-  randomCat = round(random());
-  print(randomCat)
+  rand = round(random());
 }
 
 function draw() {
+
+  m = month();
+  d = day();
+  s = second();
+
   background(220);
-  image(imgRocket, 0, 0, imgRocketSize, imgRocketSize/imgRocketScalar);
-  image(imgMoon, 0, 0, imgMoonSize, imgMoonSize/imgMoonScalar);
-  if(pressed) {
-    image(imgMonster, imgPlaceX, imgPlaceY, imgMonsterSize, imgMonsterSize/imgMonsterScalar);
-  }
-
-  if(randomCat){
-    image(imgCat, 0, height/2);
-  } else {
-    image(imgElmo, 0, height/2);
-  }
   
+  if (myIP == "167.96.61.244") {
+    image(jack, 0, 0)
+
+  } else {
+
+    image(lorem, 0, 0)
+
+    // image(pump, 0, 0, 300, 300)
+  }
+
+  print(myIP)
 }
 
-function mousePressed() {
-  imgPlaceX = mouseX;
-  imgPlaceY = mouseY;
-  pressed = true;
-  print("hi")
-}
+document.addEventListener("DOMContentLoaded", function() {
+  // Fetch the IP address from the API
+  fetch("https://api.ipify.org?format=json")
+      .then(response => response.json())
+      .then(data => {
+          // Display the IP address on the screen
+          myIP = data.ip;
+      })
+      .catch(error => {
+          console.error("Error fetching IP address:", error);
+      });
+});
